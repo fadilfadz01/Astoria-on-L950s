e46b6ad624fbc860ec49067cb728d4c715a93fc4
Fadil Fadz <66063294+fadilfadz01@users.noreply.github.com>
2023-08-30 17:51:54 +0530

    Talkman support

--- Install.bat
+++ Install.bat
@@ -1,7 +1,19 @@
 @echo off
-"%~dp0"thor2.exe -mode uefiflash -ffufile "%~dp0RM1085_1072_prod_ready.ffu"
+title Astoria on L950s
+echo Get Astoria working on L950s
+echo https://github.com/fadilfadz01/Astoria-on-L950s
+echo.
+echo Select the device model [1/2]
+echo 1. Lumia 950 XL
+echo 2. Lumia 950
+choice /n /c 12 /m "> "
+if errorlevel 2 set device=Talkman&goto continue
+if errorlevel 1 set device=Cityman&goto continue
+:continue
+echo.
+"%~dp0"thor2.exe -mode uefiflash -ffufile "%~dp0flash.ffu"
 if %errorlevel% neq 0 pause&exit
-"%~dp0"WPinternals\WPinternals.exe -FlashPartition EFIESP "%~dp0EFIESP.bin"
+if %device% == Cityman ("%~dp0"WPinternals\WPinternals.exe -FlashCustomROM "%~dp0Devices\%device%\EFIESP.zip") else ("%~dp0"WPinternals\WPinternals.exe -FlashCustomROM "%~dp0Devices\%device%\EFIESP.zip")
 if %errorlevel% neq 0 pause&exit
 "%~dp0"WPinternals\WPinternals.exe -EnableTestSigning
 if %errorlevel% neq 0 pause&exit
@@ -21,9 +33,33 @@ bcdedit /store "%MainOS%\EFIESP\EFI\Microsoft\BOOT\BCD" /set {bootmgr} displaybo
 bcdedit /store "%MainOS%\EFIESP\EFI\Microsoft\BOOT\BCD" /set {bootmgr} timeout 30
 bcdedit /store "%MainOS%\EFIESP\EFI\Microsoft\BOOT\BCD" /set {bootmgr} displayorder "{default}" "{0d1b5e40-42f1-41e7-a690-8dd3ce23cc11}"
 bcdedit /store "%MainOS%\EFIESP\EFI\Microsoft\BOOT\BCD" /set {default} debug on
-if %errorlevel% neq 0 pause&exit
+reg load HKLM\LSOFTWARE "%MainOS%\Windows\System32\Config\SOFTWARE"
+reg add HKLM\LSOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock /v AllowDevelopmentWithoutDevLicense /t REG_DWORD /d 1 /f
+reg add HKLM\LSOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock /v AllowAllTrustedApps /t REG_DWORD /d 1 /f
+reg add HKLM\LSOFTWARE\Microsoft\Windows\CurrentVersion\DeviceUpdate\Agent\Settings /v GuidOfCategoryToScan /t REG_SZ /d 00000000-0000-0000-0000-000000000000 /f
+reg unload HKLM\LSOFTWARE
+if not exist "%MainOS%\Windows\Packages\registryFiles\OEMSettings.reg" goto Skip
+attrib -s "%MainOS%\Windows\Packages\registryFiles\OEMSettings.reg"
+ren "%MainOS%\Windows\Packages\registryFiles\OEMSettings.reg" "OEMSettings.gz"
+"%~dp07za.exe" e "%MainOS%\Windows\Packages\registryFiles\OEMSettings.gz" -o"%MainOS%\Windows\Packages\registryFiles\"
+cmd /u /c echo.>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings"
+cmd /u /c echo>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings" [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock]
+cmd /u /c echo>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings" "AllowAllTrustedApps"=dword:00000001
+cmd /u /c echo>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings" "AllowDevelopmentWithoutDevLicense"=dword:00000001
+cmd /u /c echo.>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings"
+cmd /u /c echo>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings" [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\DeviceUpdate\Agent\Settings]
+cmd /u /c echo>>"%MainOS%\Windows\Packages\registryFiles\OEMSettings" "GuidOfCategoryToScan"="00000000-0000-0000-0000-000000000000"
+"%~dp07za.exe" u "%MainOS%\Windows\Packages\registryFiles\OEMSettings.gz" "%MainOS%\Windows\Packages\registryFiles\OEMSettings"
+ren "%MainOS%\Windows\Packages\registryFiles\OEMSettings.gz" "OEMSettings.reg"
+attrib +s "%MainOS%\Windows\Packages\registryFiles\OEMSettings.reg"
+del "%MainOS%\Windows\Packages\registryFiles\OEMSettings"
+:Skip
 xcopy /cey "%~dp0System32" "%MainOS%\Windows\System32\"
+if %device% == Cityman xcopy /cey "%~dp0Devices\Cityman\Config" "%MainOS%\Windows\System32\Config\"
 if %errorlevel% neq 0 pause&exit
+echo Keep the phone connected to the PC.
+echo You can disconnect once the phone booted to the OS.
+echo Reboot the phone manually by pressing and holding the power-button of the phone for about 10 seconds until it vibrates.
 "%~dp0"Windbg\kd.exe -k usb:targetname=WOATARGET
 pause
 exit
--- README.md
+++ README.md
@@ -1,25 +1,31 @@
-# Astoria on L950XL
-Get Astoria working on Lumia 950 XL.
+# Astoria on L950s
+Get Astoria working on Lumia 950s.
 
 
 ### Disclaimer
 >* I'm not responsible for any damage that may occur to your device.
 >* By using this script and flashing Astoria will permanently wipe your data.
->* This will not work with Lumia 950 or any other device.
+>* This will not work with any other devices.
 
 
 ### Pre-Installation
-1. Unlock the Boootloader using WPinternals.
-2. Download and place the Astoria [FFU](https://mega.nz/file/c5pG2aRK#8_MzaEaq-HSOEDFh_BELfeNpctACxXJn_bDtKylwtjQ) in the script folder. (Make sure the FFU name is "RM1085_1072_prod_ready.ffu")
+1. Unlock the Boootloader using [WPinternals](https://github.com/ReneLergner/WPinternals/tree/master/WPinternals).
+2. Download and place the Astoria [FFU](https://mega.nz/folder/p0gFQT7R#_3aRCm8smFMUumArqnjNdQ) to the script folder.
+3. Rename the FFU to "flash.ffu".
 
 
 ### Installation
 1. Run the "Install.bat" as `ADMINISTRATOR`.
-2. Input the MainOS partition letter when it asks. (The script will boot the device to Mass Storage Mode itself)
-3. Reboot your device without unplugging it from the PC when you see "Waiting to reconnect..." in the console.
-4. Set the date to `14 Oct 2015` in the OOBE process.
+2. Select your device model from the console list.
+
+	`Note: In case your deice boots into a blue screen, force reboot your device without unplugging it from the PC.`
+
+3. Input the `MainOS` partition letter when it asks. (The script will boot the device to Mass Storage Mode itself)
+4. Reboot your device without unplugging it from the PC when you see "Waiting to reconnect..." in the console.
+5. Once the device is booted, set the date to `14 Oct 2015` in the OOBE setup process.
 
 
 ### Post-Installation
 1. Boot to Mass Storage Mode.
-2. Run command "bcdedit /store {MainOS}\EFIESP\EFI\Microsoft\BOOT\BCD /set {default} debug off". (Replace `{MainOS}` with the MainOS partition letter)
+2. Run command in the CMD, "bcdedit /store {MainOS}\EFIESP\EFI\Microsoft\BOOT\BCD /set {default} debug off". (Replace `{MainOS}` with the MainOS partition letter)
+3. Reboot.
